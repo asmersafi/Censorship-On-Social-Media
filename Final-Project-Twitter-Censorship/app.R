646a926cfdf7bdcbcf6edd80d9b1fe4f4d739b7f
Asmer Safi <you@example.com>
2020-04-26 07:46:05 -0400

    Commit 8

--- Final-Project-Twitter-Censorship/app.R
+++ Final-Project-Twitter-Censorship/app.R
@@ -8,65 +8,62 @@
 #
 
 library(shiny)
+library(giphyr)
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
-    "Country-specific Censorship on Twitter",
-    tabPanel("Model",
-             fluidPage(
-                 titlePanel("Model Title"),
-                 sidebarLayout(
-                     sidebarPanel(
-                         selectInput(
-                             "plot_type",
-                             "Plot Type",
-                             c("Option A" = "a", "Option B" = "b")
-                         )),
-                     mainPanel(plotOutput("line_plot")))
-             )),
-    tabPanel("Discussion",
-             titlePanel("Discussion Title"),
-             p("Tour of the modeling choices you made and 
-              an explanation of why you made them")),
-    tabPanel("About", 
-             titlePanel("About"),
-             includeHTML("about.html")))
+  "Government Content Censorship on Twitter",
+  
+  
+  tabPanel("About", 
+           includeHTML("about.html")),
+  tabPanel("Data",
+           includeHTML("data.html")),
+  
+  tabPanel("Model",
+           fluidPage(
+             titlePanel("Content Removal Requests - an Overview"),
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(
+                   "plot_type",
+                   "Plot Type",
+                   c("Total Requests Specified for Withdrawal  - All Countries" = "a", "Total Content Withheld - All Countries" = "b")
+                 )),
+               mainPanel(plotOutput("line_plot")))
+           ))
+  
+)
 
-function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
 
 server <- function(input, output) {
-    output$line_plot <- renderPlot({
-        # Generate type based on input$plot_type from ui
-        
-        ifelse(
-            input$plot_type == "a",
-            
-            # If input$plot_type is "a", plot histogram of "waiting" column 
-            # from the faithful dataframe
-            
-            x   <- faithful[, 2],
-            
-            # If input$plot_type is "b", plot histogram of "eruptions" column
-            # from the faithful dataframe
-            
-            x   <- faithful[, 1]
-        )
-        
-        # Draw the histogram with the specified number of bins
-        
-        hist(x, col = 'darkgray', border = 'white')
-    })
+  output$line_plot <- renderImage({
+    # Generate type based on input$plot_type from ui
+    
+    ifelse(
+      input$plot_type == "a",
+      
+      # If input$plot_type is "a", plot histogram of "waiting" column 
+      # from the faithful dataframe
+      
+      list(src = "Plot4.jpg",
+           contentType = 'image/jpg',
+           width = 1000,
+           height = 700),
+      
+      # If input$plot_type is "b", plot histogram of "eruptions" column
+      # from the faithful dataframe
+      
+      list(src = "Plot5.jpg",
+           contentType = 'image/jpg',
+           width = 1000,
+           height = 700)
+    )
+    
+    # Draw the histogram with the specified number of bins
+    
+  })
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
